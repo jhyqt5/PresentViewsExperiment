3bcc7abb95cf8e16160491bc818e6972edb1c4ee
Dan Huang <jhyqt5@gmail.com>
2016-06-09 12:40:42 -0600

    feat: Add view experiments

--- PresentViewsExperiment/ViewController.swift
+++ PresentViewsExperiment/ViewController.swift
@@ -8,18 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    
+   let imagePicker = UIImagePickerController()
+    
+    @IBAction func openImagePicker(sender: UIButton) {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .PhotoLibrary
+        
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+
+    @IBAction func activityView(sender: UIButton) {
+        let activityViewController = UIActivityViewController(activityItems: ["hello"], applicationActivities: nil)
+        
+        presentViewController(activityViewController, animated: true, completion: nil)
+    }
+    
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        imagePicker.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
